5627da27ff934d4bceb258e1e1aef79a14815573
ChetaOk <chetachukwu.okonkwo@pau.edu.ng>
2021-11-15 18:48:44 +0100

    First C Exercises

---
+++ C_Exercises/Exercise-1.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(){
+	printf("C Programming is a powerful general-purpose language. \n");
+	printf("It is an excellent language to learn to program. \n");
+	return 0;
+}
